24153d3f6faccdd543e2fb1ae75f5a80310552c3
wasiabdul <wasi.abdul@gmail.com>
2016-04-09 19:16:41 +0700

    Add project files.

---
+++ Test BOOX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_BOOX.Startup))]
+namespace Test_BOOX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
